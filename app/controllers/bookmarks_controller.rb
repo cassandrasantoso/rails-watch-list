f33d9a3b3886e04bcb627c276dbc75a3c06c6330
Cassandra Santoso <cassandra.santoso04@gmail.com>
2026-07-11 15:47:20 +0900

    Rough version of the app without reviews

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,10 +1,37 @@
 class BookmarksController < ApplicationController
+  before_action :set_list, only: [ :new, :create ]
+  before_action :set_bookmark, only: [ :destroy ]
+
   def new
+    @bookmark = Bookmark.new
   end
 
   def create
+    @bookmark = @list.bookmarks.new(bookmark_params)
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @list = @bookmark.list
+    @bookmark.destroy
+    redirect_to list_path(@list)
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:movie_id, :comment)
   end
 end
